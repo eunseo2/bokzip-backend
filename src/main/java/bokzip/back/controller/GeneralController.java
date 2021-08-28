38346335c7a13645a2308ac9fcbe5bbf42515cc0
eunseo2 <20180972@sungshin.ac.kr>
2021-08-28 15:37:22 +0900

    [mod:#3]Generals 요청에 따른 응답코드 추가

--- src/main/java/bokzip/back/controller/GeneralController.java
+++ src/main/java/bokzip/back/controller/GeneralController.java
@@ -1,7 +1,10 @@
 package bokzip.back.controller;
 
 import bokzip.back.domain.General;
+import bokzip.back.dto.ErrorResponse;
 import bokzip.back.service.GeneralService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,8 +27,18 @@ public class GeneralController {
 
     //@param : pk로 일반 데이터 조회
     @GetMapping("/general/{id}")
-    public Optional<General> findById(@PathVariable Long id) {
-        return generalService.findById(id);
+    public ResponseEntity findById(@PathVariable Long id) {
+        Optional<General> general = generalService.findById(id);
+
+        if(id >= 100 || id <= 0){
+            return new ResponseEntity<>(
+                    ErrorResponse.res("일치하는 정보가 없습니다. general_id를 다시 확인해주십시오."), HttpStatus.BAD_REQUEST);
+        }else if(!general.isPresent()){ //404 Not Found
+            return new  ResponseEntity<>(ErrorResponse.res("요청받은 리소스를 찾을 수 없습니다."), HttpStatus.NOT_FOUND);
+        }else{ //200 OK
+            return new  ResponseEntity<>(general, HttpStatus.OK);
+        }
+
     }
 
 }
