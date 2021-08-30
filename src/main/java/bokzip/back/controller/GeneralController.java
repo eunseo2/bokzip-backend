2acc6cc679698919c9084e8f6875c2a3080dbb25
eunseo2 <20180972@sungshin.ac.kr>
2021-08-30 21:37:24 +0900

    [mod:#6] 인기순, 조회순 정렬 추가

--- src/main/java/bokzip/back/controller/GeneralController.java
+++ src/main/java/bokzip/back/controller/GeneralController.java
@@ -1,10 +1,7 @@
 package bokzip.back.controller;
 
 import bokzip.back.domain.General;
-import bokzip.back.dto.ErrorResponse;
 import bokzip.back.service.GeneralService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,25 +17,29 @@ public class GeneralController {
     }
 
     //@param : 일반 전체 데이터 조회
-    @GetMapping("/general")
+    @GetMapping("/generals")
     public List<General> generals() {
         return generalService.findAll();
     }
 
+    //@param : 일반 전체 데이터 조회
+    @GetMapping("/generals/star")
+    public List<General> StarGenerals() {
+        return generalService.StarfindAll();
+    }
+
+
+    //@param : 일반 전체 데이터 조회
+    @GetMapping("/generals/view")
+    public List<General> ViewGenerals() {
+        return generalService.ViewfindAll();
+    }
+
+
     //@param : pk로 일반 데이터 조회
     @GetMapping("/general/{id}")
-    public ResponseEntity findById(@PathVariable Long id) {
-        Optional<General> general = generalService.findById(id);
-
-        if (id >= 100 || id <= 0) {
-            return new ResponseEntity<>(
-                    ErrorResponse.res("일치하는 정보가 없습니다. general_id를 다시 확인해주십시오."), HttpStatus.BAD_REQUEST);
-        } else if (!general.isPresent()) { //404 Not Found
-            return new ResponseEntity<>(ErrorResponse.res("요청받은 리소스를 찾을 수 없습니다."), HttpStatus.NOT_FOUND);
-        } else { //200 OK
-            return new ResponseEntity<>(general, HttpStatus.OK);
-        }
-
+    public Optional<General> findById(@PathVariable Long id) {
+        return generalService.findById(id);
     }
 
 }
--- src/main/java/bokzip/back/service/GeneralService.java
+++ src/main/java/bokzip/back/service/GeneralService.java
@@ -2,6 +2,7 @@ package bokzip.back.service;
 
 import bokzip.back.domain.General;
 import bokzip.back.repository.GeneralRepository;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,15 +12,23 @@ import java.util.Optional;
 public class GeneralService {
     private final GeneralRepository generalRepository;
 
-    public GeneralService(GeneralRepository generalRepository){
+    public GeneralService(GeneralRepository generalRepository) {
         this.generalRepository = generalRepository;
     }
 
-    public Optional<General>findById(Long id){
+    public Optional<General> findById(Long id) {
         return generalRepository.findById(id);
     }
 
-    public List<General> findAll(){
-        return generalRepository.findAll();
+    public List<General> findAll() {
+        return generalRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
+    }
+
+    public List<General> StarfindAll() {
+        return generalRepository.findAll(Sort.by(Sort.Direction.DESC, "starCount"));
+    }
+
+    public List<General> ViewfindAll() {
+        return generalRepository.findAll(Sort.by(Sort.Direction.DESC, "viewCount"));
     }
 }
