e1fbc5069faa809381d67c66c805e9b313fb1a23
eunseo2 <20180972@sungshin.ac.kr>
2021-09-02 12:21:42 +0900

    [feat:#26] 필터구현 및 조회수 증가 관련

--- src/main/java/bokzip/back/controller/PostController.java
+++ src/main/java/bokzip/back/controller/PostController.java
@@ -1,19 +1,19 @@
 package bokzip.back.controller;
 
 import bokzip.back.domain.Post;
-import bokzip.back.dto.HomeMapping;
+import bokzip.back.dto.PostMapping;
+import bokzip.back.dto.SortType;
 import bokzip.back.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RequestMapping("/post")
 @RestController
-public class PostController{
+public class PostController {
 
     private final PostService postService;
 
@@ -23,32 +23,35 @@ public class PostController{
     }
 
     //@param : [중앙부처 + 로컬] 전체 데이터 조회
-    @GetMapping("/center")
-    public List<HomeMapping> selectAll(){
-        return postService.findAll();}
+    @GetMapping("/centers")
+    public List<PostMapping> selectAll() {
+        return postService.findAll();
+    }
+
 
     //@param : [중앙부처 + 로컬] pk로 데이터 조회
     @GetMapping("/center/{id}")
-    public Optional<Post> getOneData(@PathVariable @Validated Long id){ //id에 validated로 선언하여 binding error 시 에러 호출
+    public Optional<Post> addOneData(@PathVariable @Validated Long id) {
         Optional<Post> post = postService.findId(id);
 
-        if(!post.isPresent()) //null 값 반환 방지
+        if (!post.isPresent()) //null 값 반환 방지
             throw new RuntimeException("404");
 
         return post;
     }
 
-    //@param : [중앙부처] category로 조회
-    @GetMapping("/center/category/{category}")
-    public List<HomeMapping> getAllCategory(@PathVariable @Validated String category){
-        List<HomeMapping> categoryResult = new ArrayList<>();
-
-        postService.getListLikeCategory(category).forEach(categoryResult::add);
-
-        if(categoryResult.isEmpty()) //null 값 반환 방지
-            throw new RuntimeException("404");
-
-        return categoryResult;
+    //@param : [중앙부처 + 로컬] 조회수 증가
+    @GetMapping("/center/view/{id}")
+    public void addPostView(@PathVariable @Validated Long id) {
+        postService.addPostView(id);
     }
 
+    //@param : [중앙부처 + 로컬] 맞춤형 정보
+    @GetMapping("/center/custom")
+    public List<PostMapping> getPosts(@RequestParam(value = "category") String category,
+                                      @RequestParam(value = "local", required = false, defaultValue = "") String local,
+                                      @RequestParam(value = "sort", required = false, defaultValue = "") SortType sort
+    ) {
+        return postService.getListCategorySort(category, sort);
+    }
 }
